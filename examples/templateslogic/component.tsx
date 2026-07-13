a92f05e7a2b69a243a58c4f7182722bf87701402
Cydonianllama <142017erick@gmail.com>
2026-07-13 02:41:04 -0500

    snippets y mejoras

---
+++ examples/templateslogic/component.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { UseAppData } from "@/hooks/app/useAppData"
+
+type MyComponentProp = {
+
+}
+
+export const MyComponent = ({  } : MyComponentProp) => {
+  const useAppData = UseAppData()
+
+  return(<>
+
+  </>)
+}
